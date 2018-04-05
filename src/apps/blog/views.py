6853c92927acbc3a5def0c178ca308f673a2c617
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-04-05 00:17:26 -0300

    Primeiro objeto criado

--- src/apps/blog/views.py
+++ src/apps/blog/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
-# from .models import Category
+from .models import Category
 
 
 def home(request):
     name = 'Rafael Ribeiro'
     languages = ['Python', 'PHP', 'Java', 'Ruby']
 
+    # Category.objects.create(name="Python");
+    categories = Category.objects.all()
+
     context = {
         'name': name,
         'languages': languages,
+        'categories': categories,
     }
 
-    # Category.objects.create(name="Python")
     return render(request, 'home.html', context)
